655e0db2989a18e2cd8795b979c57bd112b30fc3
LEEWEBSTECHNOLOGIES <aliuajibade317@gmail.com>
2021-02-18 23:14:01 -0800

    completion of blogphp project

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+<?php include('includes/carousel.php'); ?>
+
+<div class="container py-5">
+  <div class="row">
+
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h3>Our Vision</h3>
+          <p>Hello.. this is Leewebs Technologies</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h3>Our Mission</h3>
+          <p>Hello.. this is Leewebs Technologies</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h3>Our Values</h3>
+          <p>Hello.. this is Leewebs Technologies</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="container py-5">
+  <div class="row">
+
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-body">
+          <h2>Welcome to My College Website</h2>
+          <hr>
+          <p>
+            This is Leewebs Technologies for website making.
+            This is Leewebs Technologies for website making.
+            This is Leewebs Technologies for website making.
+          </p>
+        </div>
+      </div>        
+    </div>
+    
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+          <h2>Notices!</h2>
+          <hr>
+          <p>This is Leewebs Technologies for website making.</p>
+        </div>
+      </div>    
+    </div>
+  
+  
+  </div>
+</div>
+
+
+<?php include('includes/footer.php'); ?>
